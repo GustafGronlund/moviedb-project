7003a50c346233cf7c5606be1195e3f036c9a156
GustafGronlund <gustafgronlund@gmail.com>
2022-09-09 14:22:48 +0200

    fixed some css

--- src/components/PopularMovies.jsx
+++ src/components/PopularMovies.jsx
@@ -14,6 +14,24 @@ const PopularMovies = () => {
   );
   console.log("datan:", data);
 
+  const link = document.querySelectorAll(".link");
+  const hoverReveal = document.querySelectorAll(".hover-reveal");
+  const linkImages = document.querySelectorAll(".hidden-img");
+
+  for (let i = 0; i < link.length; i++) {
+    link[i].addEventListener("mousemove", (e) => {
+      hoverReveal[i].style.opacity = 1;
+      hoverReveal[i].style.transform = `translate(-100%, -50%) rotate(5deg)`;
+      linkImages[i].style.transform = "scale(1,1)";
+      hoverReveal[i].style.left = e.clientX + "px";
+    });
+    link[i].addEventListener("mouseleave", (e) => {
+      hoverReveal[i].style.opacity = 0;
+      hoverReveal[i].style.transform = `translate(-50%, -50%) rotate(-5deg)`;
+      linkImages[i].style.transform = "scale(0.8, 0.8)";
+    });
+  }
+
   if (isLoading) {
     return "it's loading";
   }
@@ -27,11 +45,22 @@ const PopularMovies = () => {
       <h4>20 most popular movies</h4>
       <section className="popularMovies">
         {data?.results.map((movie) => (
-          <div className="popular-movie-div">
-            <NavLink to={`/movie/${movie.id}`}>
-              <img src={`${imgUrl}${movie.poster_path}`} />
+          <div className="popular-movie-div link">
+            <img
+              className="popular-movie-poster"
+              src={`${imgUrl}${movie.poster_path}`}
+            />
+            <NavLink className="popular-movie-title" to={`/movie/${movie.id}`}>
+              <p key={movie.id}>{movie.original_title}</p>
             </NavLink>
-            <p key={movie.id}>{movie.original_title}</p>
+            <div className="hover-reveal image01">
+              <img
+                src={`${imgUrl}${movie.poster_path}`}
+                alt="movie-poster"
+                className="hidden-img"
+              />
+            </div>
+            <p className="popular-movie-overview">{movie.overview}</p>
           </div>
         ))}
         {/* </Marquee> */}
